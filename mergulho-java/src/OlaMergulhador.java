95eed2d3e73d8128b66290a26dc5b16040a2cd19
douglas.dasilva <douglas.dasilva@gssbr.com.br>
2023-07-18 10:26:58 -0300

    ola mergulhador - curso JM - algaworks

---
+++ mergulho-java/src/OlaMergulhador.java
@@ -0,0 +1,20 @@
+public class OlaMergulhador {
+    public static void main(String[] args) {
+        System.out.println("olá mergulhador   ");
+
+        String nomeCompleto = "Douglas Ribeiro";
+        int idade = 35;
+
+        System.out.println(nomeCompleto + ", " + idade);
+
+        double peso = 68.1d;
+        System.out.println("peso " + peso);
+
+        float taxa = 1_123.88f;
+        System.out.println("taxa " + taxa);
+
+        boolean maiorIdade = idade > 18;
+        System.out.println("maior 18 " + maiorIdade);
+
+    }
+}
