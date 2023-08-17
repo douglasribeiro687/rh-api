8d48c5c7aac7797be6ab18b77ec0e7eb563b4db2
douglas.dasilva <douglas.dasilva@gssbr.com.br>
2023-08-16 23:12:02 -0300

    testes commit no github

--- banco/src/com/algaworks/banco/app/Principal2.java
+++ banco/src/com/algaworks/banco/app/Principal2.java
@@ -2,15 +2,14 @@ package com.algaworks.banco.app;
 
 import com.algaworks.banco.modelo.Pessoa;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Principal2 {
     public static void main(String[] args) {
         Pessoa pessoa1 = new Pessoa("Douglas Ribeiro", "123.123.123-99");
         Pessoa pessoa2 = new Pessoa("Rafael Oliveira", "999.123.123-99");
+        Pessoa pessoa3 = new Pessoa("Luisa Oliveira", "999.123.123-77");
 
         // Listas
 //        List<Pessoa> pessoas = new ArrayList<>();
@@ -19,6 +18,7 @@ public class Principal2 {
         Set<Pessoa> pessoas = new HashSet<>();
         pessoas.add(pessoa1);
         pessoas.add(pessoa2);
+        pessoas.add(pessoa3);
 
         System.out.println(pessoas);
 
@@ -31,12 +31,15 @@ public class Principal2 {
         }
 
         // verificar se contem na lista
-        Pessoa pessoa3 = new Pessoa("Rafael Oliveira", "999.123.123-99");
+        Pessoa pessoa4 = new Pessoa("Daiane Lopes", "999.123.123-22");
 
         boolean existe = pessoas.contains(pessoa3);
-        System.out.println(existe);
+        System.out.println("Pessoa 3 esta contindo na lista de pessoas: " + existe);
 
-        System.out.println(pessoa2.equals(pessoa3));
+        existe = pessoas.contains(pessoa4);
+        System.out.println("Pessoa 4 esta contindo na lista de pessoas: " + existe);
+
+        System.out.println("Testar se pessoa2 é igual a pessoa 3: " + pessoa2.equals(pessoa3));
 
         // implem de conjuntos classe collections
 
